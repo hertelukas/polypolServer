4efab9cdba66584acedb0b856729e54eb935c5f8
Lukas Hertel <lukashertel@localhost.localdomain>
2020-04-11 10:28:08 +0200

    Added needed packages and structure

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace polypolServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://admin:Fz05cKoP4PPx@polypol-i4wle.mongodb.net/test?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabaseNames().ToList();
+
+            System.Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                System.Console.WriteLine(db);
+            }
+        }
+    }
+}
